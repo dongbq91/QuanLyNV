7dae54be3aabe44d866406af5f83b55aab47917f
dongbq <dongbq91@gmail.com>
2021-03-25 16:16:42 +0700

    create data

---
+++ data.sql
@@ -0,0 +1,10 @@
+Create table Nhanvien (
+	id int(10) primary key,
+    name nvarchar(30),
+    age int(10),
+    address nvarchar(30)
+);
+Create table Phongban(
+	idPB int(10) primary key,
+    namePB nvarchar(30)
+);
